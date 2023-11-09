65cc79f647a25e16263c5cd70efe46f8e6176f3d
Pascal d'Achard <0309597@student.rocvantwente.nl>
2023-11-09 22:42:01 +0100

    x

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,9 @@
     import "../app.css";
 </script>
 
-<slot/>
+<div class="bg-gray-700 h-screen fixed w-screen -z-50"></div>
+
+
+<div class="flex flex-col min-h-screen justify-center items-center text-white">
+    <slot/>
+</div>
