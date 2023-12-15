2c211cb45aa81517f7a4a813f121457ae4fd34fe
Pascal d'Achard <0309597@student.rocvantwente.nl>
2023-12-15 12:54:09 +0100

    sandbox

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,11 @@
     import "../app.css";
 </script>
 
-<div class="bg-gray-700 h-screen fixed w-screen -z-50"></div>
+<head>
+    <title>A fun little sandbox game</title>
+</head>
+
+<div class="bg-gray-950 text-white h-screen fixed w-screen -z-50"></div>
 
 
 <div class="flex flex-col min-h-screen justify-center items-center text-white">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,22 @@
 <script>
+    import sandbox_game from "$lib/sandbox/sandbox_game.js";
+    import {onMount} from "svelte";
     
+    onMount(() => {
+        sandbox_game();
+    })
 </script>
 
-<h1>Hello, world!</h1>
+<h1 class="text-3xl mb-10">A fun little sandbox game</h1>
+
+<h2 class="font-bold tracking-widest">Controls</h2>
+<div class="flex gap-8">
+    <p class="text-amber-100">1 - Sand</p>
+    <p class="text-gray-300">2 - Stone</p>
+    <p class="text-blue-300">3 - Water</p>
+    <p class="text-neutral-300">4 - Metal</p>
+    <p class="text-amber-400">5 - Soil</p>
+    <p class="text-red-300">6 - Lava</p>
+    <p class="text-white">0 - Erase</p>
+</div>
+<canvas id="sandbox-canvas"></canvas>
